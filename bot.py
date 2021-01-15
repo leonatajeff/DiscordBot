7eb846af743395cf13a7432acd144d041c7db04b
leonatajeff <leonatajeff@gmail.com>
2021-01-15 00:39:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,21 @@ import re
 
 import discord
 import spotipy
+from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public"))
+load_dotenv()
 
+#Discord Infomration
+TOKEN = os.environ['DISCORD_TOKEN']
+CHANNEL = os.environ['CHANNEL']
+
+#Spotipy Information
+SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
+SPOTIPY_REDIRECT_URI = os.environ['SPOTIPY_REDIRECT_URI']
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public"))
+playlist = os.environ['PLAYLIST']
 client = discord.Client()
 
 @client.event
@@ -25,19 +36,19 @@ async def on_message(message):
             try:
                 sp.user_playlist_remove_all_occurrences_of_tracks(sp.me(), playlist, [message.content])
                 sp.playlist_add_items(playlist, [message.content])
-                print("Song added!")
+                await message.channel.send("Song added!")
             except:
-                print("Addition of song failed")
+                await message.channel.send("Addition of song failed")
         elif re.match(r'^(!remove)', message.content):
             try:
                 sp.user_playlist_remove_all_occurrences_of_tracks(sp.me(), playlist, [message.content])
-                print("Removal successful")
+                await message.channel.send("Removal successful")
             except:
-                print("Removal failed")
+                await message.channel.send("Removal failed")
         elif message.content == '!help':
             await message.channel.send("!remove [track_URL] removes tracks. \n Otherwise, just paste urls in here and it will be added")
         elif message.content == '!playlist':
-            await message.channel.send("https://open.spotify.com/playlist/0kvLobnBZgBn6nvS9kGNR9?si=2aClGxxiRp-EUGbERxWcrg")
+            await message.channel.send(playlist)
         else:
             print(f'Message id {message.id} is not a spotify link')
         return
